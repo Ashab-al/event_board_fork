090f1f49f347b09bbafd45f5254a4b9d30c5d289
allenonrails <allenonrails@zohomail.eu>
2022-03-26 12:52:39 +0300

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = [
+  {
+    name: 'Oleg',
+    email: 'oleg@mail.ru',
+    password: '123abc123',
+    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorum velit! Similique, sapiente cupiditate. Nesciunt atque praesentium, natus qui facere quam maiores accusantium facilis assumenda aut, enim deserunt ex deleniti!",
+    password_confirmation: '123abc123'
+  },
+  {
+    name: 'Admin',
+    email: 'admin@mail.ru',
+    password: '123abc123',
+    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorum velit! Similique, sapiente cupiditate. Nesciunt atque praesentium, natus qui facere quam maiores accusantium facilis assumenda aut, enim deserunt ex deleniti!",
+    password_confirmation: '123abc123' 
+  },
+  {
+    name: 'Igor',
+    email: 'igot@mail.ru',
+    password: '123abc123',
+    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorum velit! Similique, sapiente cupiditate. Nesciunt atque praesentium, natus qui facere quam maiores accusantium facilis assumenda aut, enim deserunt ex deleniti!",
+    password_confirmation: '123abc123' 
+  },
+]
+
+users.each { |user_hash| User.create(user_hash)}
+
+events = [
+  {
+    title: 'Rails online conf',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorum velit! Similique, sapiente cupiditate. Nesciunt atque praesentium, natus qui facere quam maiores accusantium facilis assumenda aut, enim deserunt ex deleniti!',
+    address: 'Minsk',
+    datetime: DateTime.now + 10,
+    user: User.first
+  }
+]
+
+events.each { |events_hash| Event.create(events_hash)}
+
+subscriptions = [
+  {
+    user: User.last,
+    event: Event.first
+  },
+  {
+    user_name: 'Alex',
+    user_email: 'mys@mauil.ru',
+    event: Event.first
+  }
+]
+
+subscriptions.each { |subscriptions_hash| Subscription.create(subscriptions_hash)}
+
+articles = [
+  {
+    title: 'This event was so cool!',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sint.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus, aspernatur.',
+    picture: 'https://tinypng.com/images/social/website.jpg',
+    event: Event.first,
+    user: Event.first.subscribers.first
+  }
+]
+
+articles.each { |articles_hash| Article.create(articles_hash)}
+
+comments = [
+  {
+    user: User.first,
+    body: 'Hiiiiiiiiiiiiiiii',
+    article: Article.first
+  },
+  {
+    user_name: 'Alex',
+    body: 'Anonym user',
+    article: Article.first
+  },
+  {
+    user: Article.first.user,
+    body: 'Author comment',
+    article: Article.first
+  }
+]
+
+comments.each { |comments_hash| Comment.create(comments_hash)}
